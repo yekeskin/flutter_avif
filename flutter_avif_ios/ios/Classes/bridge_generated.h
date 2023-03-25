a57693ebeb1a2cf2a566cad7631481492c4ba6bf
Yunus Emre Keskin <yekeskin@aol.com>
2023-03-25 12:54:26 -0400

    update generated code

--- flutter_avif_ios/ios/Classes/bridge_generated.h
+++ flutter_avif_ios/ios/Classes/bridge_generated.h
@@ -1,6 +1,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+typedef struct _Dart_Handle* Dart_Handle;
+
+typedef struct DartCObject DartCObject;
+
+typedef int64_t DartPort;
+
+typedef bool (*DartPostCObjectFnType)(DartPort port_id, void *message);
 
 typedef struct wire_uint_8_list {
   uint8_t *ptr;
@@ -17,15 +24,17 @@ typedef struct wire_list_encode_frame {
   int32_t len;
 } wire_list_encode_frame;
 
-typedef struct WireSyncReturnStruct {
-  uint8_t *ptr;
-  int32_t len;
-  bool success;
-} WireSyncReturnStruct;
+typedef struct DartCObject *WireSyncReturn;
 
-typedef int64_t DartPort;
+void store_dart_post_cobject(DartPostCObjectFnType ptr);
 
-typedef bool (*DartPostCObjectFnType)(DartPort port_id, void *message);
+Dart_Handle get_dart_object(uintptr_t ptr);
+
+void drop_dart_object(uintptr_t ptr);
+
+uintptr_t new_dart_opaque(Dart_Handle handle);
+
+intptr_t init_frb_dart_api_dl(void *obj);
 
 void wire_init_memory_decoder(int64_t port_,
                               struct wire_uint_8_list *key,
@@ -49,13 +58,11 @@ void wire_encode_avif(int64_t port_,
                       int32_t min_quantizer_alpha,
                       struct wire_list_encode_frame *image_sequence);
 
-struct wire_list_encode_frame *new_list_encode_frame(int32_t len);
+struct wire_list_encode_frame *new_list_encode_frame_0(int32_t len);
 
-struct wire_uint_8_list *new_uint_8_list(int32_t len);
+struct wire_uint_8_list *new_uint_8_list_0(int32_t len);
 
-void free_WireSyncReturnStruct(struct WireSyncReturnStruct val);
-
-void store_dart_post_cobject(DartPostCObjectFnType ptr);
+void free_WireSyncReturn(WireSyncReturn ptr);
 
 static int64_t dummy_method_to_enforce_bundling(void) {
     int64_t dummy_var = 0;
@@ -64,9 +71,12 @@ static int64_t dummy_method_to_enforce_bundling(void) {
     dummy_var ^= ((int64_t) (void*) wire_dispose_decoder);
     dummy_var ^= ((int64_t) (void*) wire_get_next_frame);
     dummy_var ^= ((int64_t) (void*) wire_encode_avif);
-    dummy_var ^= ((int64_t) (void*) new_list_encode_frame);
-    dummy_var ^= ((int64_t) (void*) new_uint_8_list);
-    dummy_var ^= ((int64_t) (void*) free_WireSyncReturnStruct);
+    dummy_var ^= ((int64_t) (void*) new_list_encode_frame_0);
+    dummy_var ^= ((int64_t) (void*) new_uint_8_list_0);
+    dummy_var ^= ((int64_t) (void*) free_WireSyncReturn);
     dummy_var ^= ((int64_t) (void*) store_dart_post_cobject);
+    dummy_var ^= ((int64_t) (void*) get_dart_object);
+    dummy_var ^= ((int64_t) (void*) drop_dart_object);
+    dummy_var ^= ((int64_t) (void*) new_dart_opaque);
     return dummy_var;
 }
