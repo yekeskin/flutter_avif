25d143a54843dc011c51a20869464d447272c4e6
Yunus Emre Keskin <yekeskin@aol.com>
2023-06-14 17:12:28 -0400

    remove unused code

--- flutter_avif/test/flutter_avif_test.dart
+++ flutter_avif/test/flutter_avif_test.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('flutter_avif');
-
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {});
